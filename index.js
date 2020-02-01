653520d702efe3cde5c2ae4492ac4de429e42b64
carneirovitor <viitor.augusto@gmail.com>
2020-02-01 14:48:35 -0300

    Preparando o deploy para o Herokuapp

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 
 //IMPORT_MODELS
 require('./models/Lawsuits');
-//IMPORT ROUTES
-require('./routes/lawsuitRoutes')(app);
 
 const app = express();
+
+
 app.use(bodyParser.json());
+
+//IMPORT ROUTES
+require('./routes/lawsuitRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
