57953b03ba44aa48e2c9889b159683f90da4df1c
carneirovitor <viitor.augusto@gmail.com>
2020-02-01 13:26:16 -0300

    modificando index.js

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ app.use(bodyParser.json());
 //IMPORT ROUTES
 require('./routes/lawsuitRoutes')(app);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  const path = require('path');
-  app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'))
-  })
-
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
