5a1d079db3223231bb65e9c67cc9bb7e6e0b1175
carneirovitor <viitor.augusto@gmail.com>
2020-02-01 13:42:11 -0300

    Preparando o deploy para o Herokuapp

--- routes/lawsuitRoutes.js
+++ routes/lawsuitRoutes.js
@@ -1,5 +1,4 @@
 // /routes/productRoutes.js
-const mongoose = require('mongoose');
 const Lawsuit = mongoose.model('lawsuits');
 const LawsuitScraper = require('../lawsuits/jbscraper')
 const path = require('path');
@@ -10,40 +9,8 @@ module.exports = async (app) => {
     //let lawsuits = await Lawsuit.find();
     //return res.status(200).send(lawsuits);
     const lawsuit = await LawsuitScraper.callScraper(req.query.lawsuitNumber)
-    return res.send(lawsuit);
+    return res.send.json(lawsuit);
   });
 
-  app.post(`/api/lawsuits`, async (req, res) => {
-    let lawsuit = await Lawsuit.create(req.body);
-    return res.status(201).send({
-      error: false,
-      lawsuit
-    })
-  })
-
-
-  app.put(`/api/lawsuits/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let lawsuit = await Lawsuit.findByIdAndUpdate(id, req.body);
-
-    return res.status(202).send({
-      error: false,
-      lawsuit
-    })
-
-  });
-
-  app.delete(`/api/lawsuit/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let lawsuit = await Lawsuit.findByIdAndDelete(id);
-
-    return res.status(202).send({
-      error: false,
-      lawsuit
-    })
-
-  })
 
 }
